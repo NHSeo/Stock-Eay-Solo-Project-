4b38f41b40d0169686989e4d10353d4165eefb1b
Noah Seo <sht6205@gmail.com>
2024-08-13 10:23:48 -0500

    Updated database

--- database.sql
+++ database.sql
@@ -7,3 +7,19 @@ CREATE TABLE "user" (
     "username" VARCHAR (80) UNIQUE NOT NULL,
     "password" VARCHAR (1000) NOT NULL
 );
+
+CREATE TABLE items (
+    "id" SERIAL PRIMARY KEY,
+    "name" VARCHAR(255) NOT NULL,
+    "note" TEXT,
+    "category" VARCHAR(100) NOT NULL,
+    "quantity" INTEGER NOT NULL, 
+    "created_at" TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE item_changes (
+    "id" SERIAL PRIMARY KEY,
+    "item_id" INTEGER REFERENCES items(id) ON DELETE CASCADE,
+    "change" INTEGER NOT NULL,
+    "changed_at" TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
